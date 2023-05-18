77b4929ab9df9cec224ed5cd096dd544214b3204
AlexandraUgryumova <124123433+AlexandraUgryumova@users.noreply.github.com>
2023-05-18 15:55:25 +0500

    Add files via upload

---
+++ WorkDB_Ugryumova/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace WorkDB_Ugryumova
+{
+    partial class Authorization_frm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Log_txt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Pass_txt = new System.Windows.Forms.TextBox();
+            this.Input_btn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.securityDBUgryumovaDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.securityDB_UgryumovaDataSet = new WorkDB_Ugryumova.SecurityDB_UgryumovaDataSet();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDBUgryumovaDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDB_UgryumovaDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "login";
+            // 
+            // Log_txt
+            // 
+            this.Log_txt.Location = new System.Drawing.Point(24, 38);
+            this.Log_txt.Name = "Log_txt";
+            this.Log_txt.Size = new System.Drawing.Size(171, 29);
+            this.Log_txt.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "password";
+            // 
+            // Pass_txt
+            // 
+            this.Pass_txt.Location = new System.Drawing.Point(24, 94);
+            this.Pass_txt.Name = "Pass_txt";
+            this.Pass_txt.PasswordChar = '*';
+            this.Pass_txt.Size = new System.Drawing.Size(170, 29);
+            this.Pass_txt.TabIndex = 3;
+            // 
+            // Input_btn
+            // 
+            this.Input_btn.BackColor = System.Drawing.Color.MediumPurple;
+            this.Input_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Input_btn.Location = new System.Drawing.Point(24, 201);
+            this.Input_btn.Name = "Input_btn";
+            this.Input_btn.Size = new System.Drawing.Size(99, 33);
+            this.Input_btn.TabIndex = 4;
+            this.Input_btn.Text = "Sing in";
+            this.Input_btn.UseVisualStyleBackColor = false;
+            this.Input_btn.Click += new System.EventHandler(this.Input_btn_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft YaHei Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl1.Location = new System.Drawing.Point(25, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 293);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Log_txt);
+            this.tabPage1.Controls.Add(this.Input_btn);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.Pass_txt);
+            this.tabPage1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tabPage1.Location = new System.Drawing.Point(4, 30);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(752, 259);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sing in";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(752, 259);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sing up";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 21);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "confirmation";
+            this.label5.Visible = false;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumPurple;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(24, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Sing up";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "password";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(24, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(171, 29);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(171, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "login";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(738, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.GhostWhite;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 348);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 230);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // securityDBUgryumovaDataSetBindingSource
+            // 
+            this.securityDBUgryumovaDataSetBindingSource.DataSource = this.securityDB_UgryumovaDataSet;
+            this.securityDBUgryumovaDataSetBindingSource.Position = 0;
+            // 
+            // securityDB_UgryumovaDataSet
+            // 
+            this.securityDB_UgryumovaDataSet.DataSetName = "SecurityDB_UgryumovaDataSet";
+            this.securityDB_UgryumovaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Authorization_frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(812, 600);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Authorization_frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Для входа введите логин и пароль";
+            this.Load += new System.EventHandler(this.Authorization_frm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDBUgryumovaDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.securityDB_UgryumovaDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Log_txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Pass_txt;
+        private System.Windows.Forms.Button Input_btn;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource securityDBUgryumovaDataSetBindingSource;
+        private SecurityDB_UgryumovaDataSet securityDB_UgryumovaDataSet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WorkDB_Ugryumova/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WorkDB_Ugryumova
+{
+    public partial class Authorization_frm : Form
+    {
+        DateTime dt = new DateTime();
+        
+        SqlDataAdapter adptr;
+        DataTable table;
+        SqlConnection connect = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SecurityDB_Ugryumova;Integrated Security=True");
+        public Authorization_frm()
+        {
+            InitializeComponent();
+
+            TableGridView();
+        }
+        private void TableGridView()
+        {
+            connect.Open();
+            adptr = new SqlDataAdapter("select * from User_tbl", connect);
+            table = new DataTable();
+            adptr.Fill(table);
+            dataGridView1.DataSource = table;
+            connect.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Authorization_frm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dt = DateTime.Today;
+            string log = textBox1.Text;
+            string pass = textBox2.Text;
+            if(log == "" || pass == "")
+            {
+                MessageBox.Show("заполнены не все поля", "сообщение");
+            }
+            else
+            {
+                connect.Open();
+                string prov = $"SELECT Login FROM dbo.User_tbl WHERE Login = {log}";
+                adptr = new SqlDataAdapter(prov, connect);
+                DataTable tables = new DataTable();
+                adptr.Fill(tables);
+                if (tables.Rows.Count > 0) MessageBox.Show("данный логин уже зарезервирован другим пользователем", "сообщение");
+                else
+                {
+                    string query = $"INSERT INTO dbo.User_tbl (login,  password,  Count,  date,  active,   role) VALUES('{log}','{pass}',0,'{dt.ToString("yyyy-MM-dd")}','True','user')";
+                    adptr = new SqlDataAdapter(query, connect);
+                    table = new DataTable();
+                    adptr.Fill(table);
+                    dataGridView1.DataSource = table;
+                    TableGridView();
+                }
+                connect.Close();
+            }
+        }
+
+        private void Input_btn_Click(object sender, EventArgs e)
+        {
+            string log = Log_txt.Text;
+            string pass = Pass_txt.Text;
+            if(log == "" || pass == "")
+            {
+                MessageBox.Show("заполнены не все поля", "сообщение");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
